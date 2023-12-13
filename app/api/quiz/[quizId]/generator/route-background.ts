3f599b40607eb013fbef9e53dfea8edd42a9ca55
Akmal <akmalulimanliari10@gmail.com>
2023-12-14 00:12:13 +0800

    add background to file name to tes netlify

--- app/api/quiz/[quizId]/generator/route-background.ts
+++ app/api/quiz/[quizId]/generator/route-background.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
-import { multipleChoice } from '@/lib/openaimc';
+import { multipleChoice } from '@/lib/openaimc-background';
 
 export const POST = async (
   req: Request,
