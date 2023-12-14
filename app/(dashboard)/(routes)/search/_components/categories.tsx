7e79a91775e4434e1a1d684316c9db43ceb8359c
Akmal <akmalulimanliari10@gmail.com>
2023-12-14 17:44:32 +0800

    add category button

---
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { Category } from '@prisma/client';
+import {
+  FcAreaChart, //
+  FcBiotech, // Biologi
+  FcBarChart, // Matematika
+  FcConferenceCall, //Sosiologi
+  FcLibrary, //
+  FcMindMap, // Kimia
+  FcGlobe, // Geografi
+  FcFlashOn, // Fisika
+  FcIdea, // Seni
+  FcFaq, // Bahasa
+  FcEnteringHeavenAlive, // Agama
+  FcButtingIn, // Olahraga
+} from 'react-icons/fc';
+
+import { IconType } from 'react-icons';
+import CategoryItem from './category-item';
+interface CategoriesProps {
+  items: Category[];
+}
+const iconMap: Record<Category['name'], IconType> = {
+  Kewarganegaraan: FcLibrary,
+  Ekonomi: FcAreaChart,
+  Biologi: FcBiotech,
+  Matematika: FcBarChart,
+  Geografi: FcGlobe,
+  Fisika: FcFlashOn,
+  Bahasa: FcFaq,
+  Agama: FcEnteringHeavenAlive,
+  Olahraga: FcButtingIn,
+  Seni: FcIdea,
+  Kimia: FcMindMap,
+  Sosiologi: FcConferenceCall,
+};
+const Categories = ({ items }: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
