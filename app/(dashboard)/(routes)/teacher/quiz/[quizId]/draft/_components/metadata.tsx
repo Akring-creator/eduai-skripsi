9fd2dc7c6fb32870baf07f6eeafff77a97e3fce2
Akmal <akmalulimanliari10@gmail.com>
2023-12-16 22:11:15 +0800

    move export_components

--- app/(dashboard)/(routes)/teacher/quiz/[quizId]/draft/_components/metadata.tsx
+++ app/(dashboard)/(routes)/teacher/quiz/[quizId]/draft/_components/metadata.tsx
@@ -21,7 +21,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 import { useRouter } from 'next/navigation';
-import ExportToWordHandler from './export-button';
+import ExportToWordHandler from '../../export/_components/export-button';
 import { auth } from '@clerk/nextjs';
 import Link from 'next/link';
 import { useState } from 'react';
