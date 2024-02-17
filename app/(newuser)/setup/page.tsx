f25e31b88d1a987f516a3cf1693e3cd9cd4b0c15
Akmal <akmalulimanliari10@gmail.com>
2024-02-17 22:30:00 +0800

    delete newUser Folder

--- app/(newuser)/setup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { db } from '@/lib/db';
-import { redirect } from 'next/navigation';
-import SetupPage from './_components/username-form';
-
-const SetupMainPage = async () => {
-  return <SetupPage />;
-};
-
-export default SetupMainPage;
